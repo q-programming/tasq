5871b98c9be0cbe070a736799e7444d52986c18c
Jakub Romaniszyn <khobar@interia.pl>
2016-09-30 19:59:02 +0200

    TASQ-295: Fix NPE thrown when serving saved request to generalError

--- src/main/java/com/qprogramming/tasq/signin/LocaleSettingAuthenticationSuccessHandler.java
+++ src/main/java/com/qprogramming/tasq/signin/LocaleSettingAuthenticationSuccessHandler.java
@@ -1,12 +1,6 @@
 package com.qprogramming.tasq.signin;
 
-import java.io.IOException;
-import java.util.Locale;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.qprogramming.tasq.support.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -15,28 +9,33 @@ import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import com.qprogramming.tasq.support.Utils;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Locale;
 
 @Component("localeSettingAuthenticationSuccessHandler")
 public class LocaleSettingAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-	@Autowired
-	private SessionLocaleResolver localeResolver;
+    @Autowired
+    private SessionLocaleResolver localeResolver;
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) throws ServletException, IOException {
-		// Eliminate signin error on saved request
-		SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
-		if (savedRequest != null) {
-			String[] redirectUlr = savedRequest.getRedirectUrl().split("/");
-			if (redirectUlr[redirectUlr.length - 1].equals("sigin")) {
-				new HttpSessionRequestCache().removeRequest(request, response);
-			}
-		}
-		String locale = Utils.getCurrentAccount().getLanguage();
-		Locale loc = new Locale(locale);
-		localeResolver.setLocale(request, response, loc);
-		super.onAuthenticationSuccess(request, response, authentication);
-	}
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws ServletException, IOException {
+        // Eliminate signin error on saved request
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+        if (savedRequest != null) {
+            String[] redirectUlr = savedRequest.getRedirectUrl().split("/");
+            String redirectParam = redirectUlr[redirectUlr.length - 1];
+            if ("sigin".equals(redirectParam) || "generalError".equals(redirectParam)) {
+                new HttpSessionRequestCache().removeRequest(request, response);
+            }
+        }
+        String locale = Utils.getCurrentAccount().getLanguage();
+        Locale loc = new Locale(locale);
+        localeResolver.setLocale(request, response, loc);
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
 
 }
